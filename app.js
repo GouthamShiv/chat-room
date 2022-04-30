9f99e9f213f9b8b5d6c716371c94d55dc28aaa21
Goutham S <gouthamshine@gmail.com>
2022-04-30 21:23:03 +0530

    leave chat fix

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
-const { userJoin, getCurrentUser } = require('./utils/users');
+const {
+    userJoin,
+    getCurrentUser,
+    userLeave,
+    getRoomUsers,
+} = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -41,7 +46,13 @@ io.on('connection', (socket) => {
 
     // When a client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', formatMessage(name, 'A user has left the chat'));
+        const user = userLeave(socket.id);
+        if (user) {
+            io.to(user.room).emit(
+                'message',
+                formatMessage(name, `${user.username} has left the chat`)
+            );
+        }
     });
 });
 
